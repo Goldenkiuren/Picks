fd7ddb589a2b989423802b7a9518e1d2961bd5e2
jGp34 <joaoeberhardtgabriel@gmail.com>
2025-10-21 20:40:11 -0300

    modificação pequena

--- common.h
+++ common.h
@@ -4,14 +4,15 @@
 #include <stdint.h>
 
 #define TYPE_DESCOBERTA 1
-#define TYPE_REQ 2
-#define TYPE_ACK_REQ 3
+#define TYPE_REQ 3
+#define TYPE_ACK_REQ 4
+#define TYPE_ERROR_REQ 5 
 #define TYPE_ACK_DESCOBERTA 4
 
 typedef struct {
     uint16_t type;          // tipo de pacote
     uint32_t seqn;         // número de sequência
-    uint32_t dest_addr;   // endereço de ip do cliente destino
+    uint32_t dest_ip_addr;   // endereço de ip do cliente destino
     uint32_t value;       // valor de transferencia
     uint32_t balance;     // para ACKs, novo saldo      
 } packet;
